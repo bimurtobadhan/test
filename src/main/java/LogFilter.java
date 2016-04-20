db7e002832cc68783af62876aba94b39af765947
Shawrup <sksuter@gmail.com>
2016-04-20 13:04:48 +0600

    2nd Commit Complete

--- src/main/java/LogFilter.java
+++ src/main/java/LogFilter.java
@@ -1,27 +1,32 @@
-import java.io.*;
 import javax.servlet.*;
-import javax.servlet.http.*;
-import java.util.*;
 
-// Implements Filter class
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LogFilter implements Filter  {
+
+    Logger log = LoggerFactory.getLogger(LogFilter.class);
+
     public void  init(FilterConfig config)
             throws ServletException{
     }
     public void  doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws java.io.IOException, ServletException {
+//        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//        StatusPrinter.print(lc);
 
-        // Get the IP address of client machine.
-        String ipAddress = request.getRemoteAddr();
+        log.debug("Entered doFilter");
+        log.debug("Debug Request: " + request);
+        log.debug("Debug Response :" + response);
 
-        // Log the IP address and current timestamp.
-        System.out.println("IP "+ ipAddress + ", Time "
-                + new Date().toString());
-
-        // Pass request back down the filter chain
         chain.doFilter(request,response);
+
+        log.error("Entered doFilter");
+        log.error("Error Request: " + request);
+        log.error("Error Response :" + response);
     }
     public void destroy( ){
-
     }
 }
